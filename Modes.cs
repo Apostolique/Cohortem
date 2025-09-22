bb65fa9e0488c461f87f09e298f5a9dbe78787ec
Apostolique <vynecompiler@email.com>
2025-09-22 12:13:38 -0400

    Modes

---
+++ Modes.cs
@@ -0,0 +1,11 @@
+namespace Cohortem {
+    public static class Modes {
+        public static int[] Ionian = [0, 2, 2, 1, 2, 2, 2, 1];
+        public static int[] Dorian = [0, 2, 1, 2, 2, 2, 1, 2];
+        public static int[] Phrygian = [0, 1, 2, 2, 2, 1, 2, 2];
+        public static int[] Lydian = [0, 2, 2, 2, 1, 2, 2, 1];
+        public static int[] Mixolydian = [0, 2, 2, 1, 2, 2, 1, 2];
+        public static int[] Aeolian = [0, 2, 1, 2, 2, 1, 2, 2];
+        public static int[] Locrian = [0, 1, 2, 2, 1, 2, 2, 2];
+    }
+}
