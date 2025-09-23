c8409a9880e5a8397521fc220e0a912dae0f6c6b
Apostolique <vynecompiler@email.com>
2025-09-23 07:36:38 -0400

    Use absolute intervals

--- Modes.cs
+++ Modes.cs
@@ -1,11 +1,11 @@
 namespace Cohortem {
     public static class Modes {
-        public static int[] Ionian = [0, 2, 2, 1, 2, 2, 2, 1];
-        public static int[] Dorian = [0, 2, 1, 2, 2, 2, 1, 2];
-        public static int[] Phrygian = [0, 1, 2, 2, 2, 1, 2, 2];
-        public static int[] Lydian = [0, 2, 2, 2, 1, 2, 2, 1];
-        public static int[] Mixolydian = [0, 2, 2, 1, 2, 2, 1, 2];
-        public static int[] Aeolian = [0, 2, 1, 2, 2, 1, 2, 2];
-        public static int[] Locrian = [0, 1, 2, 2, 1, 2, 2, 2];
+        public static int[] Ionian = [0, 2, 4, 5, 7, 9, 11, 12];
+        public static int[] Dorian = [0, 2, 3, 5, 7, 9, 10, 12];
+        public static int[] Phrygian = [0, 1, 3, 5, 7, 8, 10, 12];
+        public static int[] Lydian = [0, 2, 4, 6, 7, 9, 11, 12];
+        public static int[] Mixolydian = [0, 2, 4, 5, 7, 9, 11, 12];
+        public static int[] Aeolian = [0, 2, 3, 5, 7, 8, 10, 12];
+        public static int[] Locrian = [0, 1, 3, 5, 6, 8, 10, 12];
     }
 }
